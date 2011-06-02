b4b701ea4d8745fd68b929eb402a7df370073099
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2011-06-02 14:04:24 +0530

    Added working sound and ripped samples from SMB\!

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 window.Processing.data = {}; //the global P.js variable container
 
+//The HTML5 javascript sound (playback) API is simple enough:
+// (kept here for posterity)
+//window.Processing.data.finish = new Audio("audio/smb_world_clear.wav");
+//window.Processing.data.finish.play();
+
 mainloop = setInterval(function(){ //the main loop, am I breaking some sort of etiquette here?
-	
+
 	currentDate = new Date();
-	
+
 	window.Processing.data.now = Date.now();
-	
+
 	//window.Processing.data.year = currentDate.getFullYear();
 	//window.Processing.data.month = currentDate.getMonth();
 	//window.Processing.data.date = currentDate.getDate();
 	//window.Processing.data.day = currentDate.getDay();
-	
+
 	window.Processing.data.hour = currentDate.getHours();
 	window.Processing.data.min = currentDate.getMinutes();
 	window.Processing.data.sec = currentDate.getSeconds();
 	window.Processing.data.ms = currentDate.getMilliseconds();
-	
+
 }, 1000);
 
 
--- readme.md
+++ readme.md
@@ -11,6 +11,7 @@ Will do soonish.
 - [JSconsole](http://jsconsole.com/)
 - [Dynamic Favicons by Remy Sharp](http://remysharp.com/2010/08/24/dynamic-favicons/)
 - [GH-Pages as Master by ChrisJacob](https://gist.github.com/833223)
-- [SetInterval Listeners with JS  by PixelMech] http://evolt.org/node/36035/?cfid=3686399&cftoken=90477234
+- [SetInterval Listeners with JS  by PixelMech](http://evolt.org/node/36035/?cfid=3686399&cftoken=90477234)
 - [Dynamic Canvas Favicon at Defender of the Favicon!] (http://www.p01.org/releases/DEFENDER_of_the_favicon/)
 - [Transparent Canvas with P.js on Stackoverflow] (http://stackoverflow.com/questions/2256110/is-it-possible-to-create-transparent-canvas)
+- [HTML5 Audio Element](http://dev.w3.org/html5/spec/video.html#attr-media-preload)
