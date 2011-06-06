076d0cd76cdd293f09afc09d592728bf62c35ebd
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2011-06-06 17:32:37 +0530

    I've been working from gh-pages all this time. Rectifying.

--- main.js
+++ main.js
@@ -1,23 +1,114 @@
 window.Processing.data = {}; //the global P.js variable container
 
-mainloop = setInterval(function(){ //the main loop, am I breaking some sort of etiquette here?
-	
-	currentDate = new Date();
-	
-	window.Processing.data.now = Date.now();
-	
-	//window.Processing.data.year = currentDate.getFullYear();
-	//window.Processing.data.month = currentDate.getMonth();
-	//window.Processing.data.date = currentDate.getDate();
-	//window.Processing.data.day = currentDate.getDay();
-	
-	window.Processing.data.hour = currentDate.getHours();
-	window.Processing.data.min = currentDate.getMinutes();
-	window.Processing.data.sec = currentDate.getSeconds();
-	window.Processing.data.ms = currentDate.getMilliseconds();
-	
+getUrlVars(); 	//reads variables passed from the url
+updateTime();	//updates the time variables, this is a clock after all
+setValues();	//sets the defualt values of necessary variables
+updateState();	//updates clock state
+
+
+main = setInterval(function(){ //the main loop, am I breaking some sort of etiquette here?
+
+	updateTime();
+	updateState();
+
 }, 1000);
 
+function updateTime() {
+    currentDate = new Date();
+    window.Processing.data.now = currentDate.valueOf();
+    window.Processing.data.elapsed = (window.Processing.data.now - window.Processing.data.starttime);
 
+    //calculates the 0~1 float that tracks progress, as per state
+    if(window.Processing.data.state == 1) {
+	    window.Processing.data.counter = window.Processing.data.elapsed / window.Processing.data.workdur;
+    } else if(window.Processing.data.state == 2) {
+	    window.Processing.data.counter = window.Processing.data.elapsed / window.Processing.data.breakdur;
+    }
 
+    //updates the preformatted pretty-text
+    updatePrettytext();
 
+}
+
+function updatePrettytext() {
+
+    //this rather ugly piece of codery composes the digital-clock string that shows
+    //string manipulation sucks in js
+    d = new Date(window.Processing.data.workdur + window.Processing.data.breakdur - window.Processing.data.elapsed - 1800000);
+    if(window.Processing.data.elapsed != null) {
+        window.Processing.data.prettytext = "";
+        if(d.getMinutes() < 10)
+            window.Processing.data.prettytext += "0";
+        window.Processing.data.prettytext += d.getMinutes();
+        window.Processing.data.prettytext += ":";
+        if(d.getSeconds() < 10)
+            window.Processing.data.prettytext += "0";
+        window.Processing.data.prettytext += d.getSeconds().toString();
+    }
+    if(window.Processing.data.prettytext == "59:59")
+	window.Processing.data.prettytext = "00:00";
+
+}
+
+function updateState() {
+    window.Processing.data.state = 1; 		//start ticking as default
+    if(window.Processing.data.elapsed > window.Processing.data.workdur)
+        window.Processing.data.state = 2;	//if worktime over, it's breaktime!
+    if(window.Processing.data.elapsed > window.Processing.data.workdur+window.Processing.data.breakdur) {
+        window.Processing.data.state = 0;	//ran out of time? then stop
+	if(window.Processing.data.repeat != 0) {
+	    window.Processing.data.state = 1;	//set to repeat? then start again!
+	    window.Processing.data.urlvars.r -= 1;
+	    setValues();
+	}
+    }
+	//the state machine
+	//	0 = inactive
+	//	1 = counting "work"
+	//	2 = counting "break"
+	//	3 = pausing "work"	pausing is in no way implemented yet, not do I want to
+}
+
+function setValues() {
+    //this converts variables passed to settings, or sets them as default
+    //this is what the code checks for...
+    //w = work time in seconds
+    //b = break time in seconds
+    //r = repeat as an integer		negative value for infinite
+    //t = time of start in ms		planned so long-term pomidoris can be bookmarked
+    //s = skin				planned for eventual skin support
+    if(window.Processing.data.urlvars.t) {
+	window.Processing.data.starttime = window.Processing.data.urlvars.t;
+    } else {
+	window.Processing.data.starttime = window.Processing.data.now.valueOf();
+    }
+    if(window.Processing.data.urlvars.s) {
+        window.Processing.data.skin = window.Processing.data.urlvars.s;
+    } else {
+	window.Processing.data.skin = "kimidori";
+    }
+    if(window.Processing.data.urlvars.r) {
+    	window.Processing.data.repeat = window.Processing.data.urlvars.r;
+    } else {
+	window.Processing.data.repeat = 0;
+    }
+    if(window.Processing.data.urlvars.w) {
+	window.Processing.data.workdur = window.Processing.data.urlvars.w * 60 * 1000;
+    } else {
+	window.Processing.data.workdur = 25 * 60 * 1000;
+    }
+    if(window.Processing.data.urlvars.b) {
+	window.Processing.data.breakdur = window.Processing.data.urlvars.b * 60 * 1000;
+    } else {
+	window.Processing.data.breakdur = 5 * 60 * 1000;
+    }
+}
+
+function getUrlVars() {
+    //reads passed variables off the url string, my favored method of data retention
+    var map = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+	    map[key] = value;
+    });
+    window.Processing.data.urlvars = map;
+}
--- readme.md
+++ readme.md
@@ -1,16 +1,36 @@
-# Can't be bothered to write this now.
+# Pomidori
 
-Will do soonish.
+A fancy-smanshy web-based Pomodoro timer and personal learning exercise.
+
+### Why is this?
+
+I love the [Pomodoro](http://en.wikipedia.org/wiki/Pomodoro_technique) “technique” and the ease of [web-based timers](http://ddg.gg/?q=pomodoro+timers).
+And since none of the existing crop of web-based pomodoro timers have all the features I want, and because I could use the exercise, I’m making one myself!
+
+[And this is it](http://5310.github.com/pomidori). Pretty huh? And it does, or will in the eventual future, feature the following:
+
+### Features, Present and Future
+
+-   Open web standards FTW! No pesky flash for audio.
+-   Pretty visuals using HTML+Javascript+CSS, and [Processing.js](http://processingjs.org/)! Throw in your own favorite JS library too!
+-   Canvas visualization as favicon!
+-   No cookie-clutter, reads settings off the url. This is undocumented at the moment, and is a bit geeky without UI.
+-   Url can be bookmarked with custom settings.
+-   Eventually, when a proper UI is implemented, it'll be possibly to pause resume and continue timers across sessions...maybe.
+-   Skinnable! Although only for those willing to geek a bit. And skin changing isn't implemented yet! This part I'll get done soonish...
+
+And I also love the [color green](http://en.wiktionary.org/wiki/midori), among others.
 
 ---
 
-### References
+### Some Useful References
 
 - [P.js Docs](http://processingjs.org/reference)
 - [MDS JS Docs](https://developer.mozilla.org/en/javascript)
 - [JSconsole](http://jsconsole.com/)
 - [Dynamic Favicons by Remy Sharp](http://remysharp.com/2010/08/24/dynamic-favicons/)
 - [GH-Pages as Master by ChrisJacob](https://gist.github.com/833223)
-- [SetInterval Listeners with JS  by PixelMech] http://evolt.org/node/36035/?cfid=3686399&cftoken=90477234
+- [SetInterval Listeners with JS  by PixelMech](http://evolt.org/node/36035/?cfid=3686399&cftoken=90477234)
 - [Dynamic Canvas Favicon at Defender of the Favicon!] (http://www.p01.org/releases/DEFENDER_of_the_favicon/)
 - [Transparent Canvas with P.js on Stackoverflow] (http://stackoverflow.com/questions/2256110/is-it-possible-to-create-transparent-canvas)
+- [HTML5 Audio Element](http://dev.w3.org/html5/spec/video.html#attr-media-preload)
