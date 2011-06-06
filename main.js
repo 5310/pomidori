302a28b21b7c17bdcd02fe79510632755c0cd9c7
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2011-06-06 21:29:07 +0530

    Added skinning support\! And duplicated the default theme in pink, which is utterly pointless to do, but serves as an example.

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
-window.Processing.data = {}; //the global P.js variable container
+function load() {
+    gdata = {}; //the global P.js variable container
 
-getUrlVars(); 	//reads variables passed from the url
-updateTime();	//updates the time variables, this is a clock after all
-setValues();	//sets the defualt values of necessary variables
-updateState();	//updates clock state
-
-
+    getUrlVars(); 	//reads variables passed from the url
+    updateTime();	//updates the time variables, this is a clock after all
+    setValues();	//sets the defualt values of necessary variables
+    updateState();	//updates clock state
+    setSkin();
+}
 
 main = setInterval(function(){ //the main loop, am I breaking some sort of etiquette here?
 
-	updateTime();
-	updateState();
+    updateTime();
+    updateState();
 
 }, 1000);
 
 function updateTime() {
     currentDate = new Date();
-    window.Processing.data.now = currentDate.valueOf();
-    window.Processing.data.elapsed = (window.Processing.data.now - window.Processing.data.starttime);
+    gdata.now = currentDate.valueOf();
+    gdata.elapsed = (gdata.now - gdata.starttime);
 
     //calculates the 0~1 float that tracks progress, as per state
-    if(window.Processing.data.state == 1) {
-	    window.Processing.data.counter = window.Processing.data.elapsed / window.Processing.data.workdur;
-    } else if(window.Processing.data.state == 2) {
-	    window.Processing.data.counter = window.Processing.data.elapsed / window.Processing.data.breakdur;
+    if(gdata.state == 1) {
+	    gdata.counter = gdata.elapsed / gdata.workdur;
+    } else if(gdata.state == 2) {
+	    gdata.counter = gdata.elapsed / gdata.breakdur;
     }
 
     //updates the preformatted pretty-text
@@ -35,31 +36,31 @@ function updatePrettytext() {
 
     //this rather ugly piece of codery composes the digital-clock string that shows
     //string manipulation sucks in js
-    d = new Date(window.Processing.data.workdur + window.Processing.data.breakdur - window.Processing.data.elapsed - 1800000);
-    if(window.Processing.data.elapsed != null) {
-        window.Processing.data.prettytext = "";
+    d = new Date(gdata.workdur + gdata.breakdur - gdata.elapsed - 1800000);
+    if(gdata.elapsed != null) {
+        gdata.prettytext = "";
         if(d.getMinutes() < 10)
-            window.Processing.data.prettytext += "0";
-        window.Processing.data.prettytext += d.getMinutes();
-        window.Processing.data.prettytext += ":";
+            gdata.prettytext += "0";
+        gdata.prettytext += d.getMinutes();
+        gdata.prettytext += ":";
         if(d.getSeconds() < 10)
-            window.Processing.data.prettytext += "0";
-        window.Processing.data.prettytext += d.getSeconds().toString();
+            gdata.prettytext += "0";
+        gdata.prettytext += d.getSeconds().toString();
     }
-    if(window.Processing.data.prettytext == "59:59")
-	window.Processing.data.prettytext = "00:00";
+    if(gdata.prettytext == "59:59")
+	gdata.prettytext = "00:00";
 
 }
 
 function updateState() {
-    window.Processing.data.state = 1; 		//start ticking as default
-    if(window.Processing.data.elapsed > window.Processing.data.workdur)
-        window.Processing.data.state = 2;	//if worktime over, it's breaktime!
-    if(window.Processing.data.elapsed > window.Processing.data.workdur+window.Processing.data.breakdur) {
-        window.Processing.data.state = 0;	//ran out of time? then stop
-	if(window.Processing.data.repeat != 0) {
-	    window.Processing.data.state = 1;	//set to repeat? then start again!
-	    window.Processing.data.urlvars.r -= 1;
+    gdata.state = 1; 		//start ticking as default
+    if(gdata.elapsed > gdata.workdur)
+        gdata.state = 2;	//if worktime over, it's breaktime!
+    if(gdata.elapsed > gdata.workdur+gdata.breakdur) {
+        gdata.state = 0;	//ran out of time? then stop
+	if(gdata.repeat != 0) {
+	    gdata.state = 1;	//set to repeat? then start again!
+	    gdata.urlvars.r -= 1;
 	    setValues();
 	}
     }
@@ -78,30 +79,30 @@ function setValues() {
     //r = repeat as an integer		negative value for infinite
     //t = time of start in ms		planned so long-term pomidoris can be bookmarked
     //s = skin				planned for eventual skin support
-    if(window.Processing.data.urlvars.t) {
-	window.Processing.data.starttime = window.Processing.data.urlvars.t;
+    if(gdata.urlvars.t) {
+	gdata.starttime = gdata.urlvars.t;
     } else {
-	window.Processing.data.starttime = window.Processing.data.now.valueOf();
+	gdata.starttime = gdata.now.valueOf();
     }
-    if(window.Processing.data.urlvars.s) {
-        window.Processing.data.skin = window.Processing.data.urlvars.s;
+    if(gdata.urlvars.s) {
+        gdata.skin = gdata.urlvars.s;
     } else {
-	window.Processing.data.skin = "kimidori";
+	gdata.skin = "kimidori";
     }
-    if(window.Processing.data.urlvars.r) {
-    	window.Processing.data.repeat = window.Processing.data.urlvars.r;
+    if(gdata.urlvars.r) {
+    	gdata.repeat = gdata.urlvars.r;
     } else {
-	window.Processing.data.repeat = 0;
+	gdata.repeat = 0;
     }
-    if(window.Processing.data.urlvars.w) {
-	window.Processing.data.workdur = window.Processing.data.urlvars.w * 60 * 1000;
+    if(gdata.urlvars.w) {
+	gdata.workdur = gdata.urlvars.w * 60 * 1000;
     } else {
-	window.Processing.data.workdur = 25 * 60 * 1000;
+	gdata.workdur = 25 * 60 * 1000;
     }
-    if(window.Processing.data.urlvars.b) {
-	window.Processing.data.breakdur = window.Processing.data.urlvars.b * 60 * 1000;
+    if(gdata.urlvars.b) {
+	gdata.breakdur = gdata.urlvars.b * 60 * 1000;
     } else {
-	window.Processing.data.breakdur = 5 * 60 * 1000;
+	gdata.breakdur = 5 * 60 * 1000;
     }
 }
 
@@ -111,5 +112,11 @@ function getUrlVars() {
     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
 	    map[key] = value;
     });
-    window.Processing.data.urlvars = map;
+    gdata.urlvars = map;
+}
+
+function setSkin() {
+    document.getElementById("stylesheet").setAttribute("href", gdata.skin+"/style.css");
+    document.getElementById("favcanvas").setAttribute("datasrc", gdata.skin+"/fav.pjs");
+    document.getElementById("vizcanvas").setAttribute("datasrc", gdata.skin+"/viz.pjs");
 }
